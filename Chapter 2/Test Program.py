2ac7b3703ad9b9c2f69f2324e8cf6e9b9a3a2e55
NoahJack143 <111085947+NoahJack143@users.noreply.github.com>
2022-08-15 14:46:12 -0500

    Update Test Program.py

--- Chapter 2/Test Program.py
+++ Chapter 2/Test Program.py
@@ -3,3 +3,11 @@
 def print_something():
     #print a message
     print("This is my message")
+    
+#another function receives no arguments
+#it just output two messages separated by
+#a blank line
+def another_function():
+    print("This is message 1")
+    print()
+    print("This is message 2")
