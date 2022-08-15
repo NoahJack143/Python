df7c3fc86a67c62636c828335581548dd4bc6133
NoahJack143 <111085947+NoahJack143@users.noreply.github.com>
2022-08-15 14:46:16 -0500

    Create Chapter 2 Programs 2022.py

---
+++ Chapter 2/Chapter 2 Programs 2022.py
@@ -0,0 +1,26 @@
+def comment_example():
+    #comment_example receives no arguments
+    #it explains how to create a function header
+    #and outputs or returns "Hello World!"
+    print("Hello World!")
+    
+def program2_1():
+    #program 2 1 receives no arguments
+    #it outputs three messages using apostrophes instead
+    #of quotation marks.
+    print("Kate Austen")
+    print("123 Full Circle Drive")
+    print("Asheville, NC 28899")
+    
+def program2_2(): #double quote output
+    print("Kate Austen")
+    print("123 FUll Circle Drive")
+    print("Asheville, NC 28899")
+    
+def program2_3(): #display apostrophe
+    print()
+    print("Don't fear!")
+    print("I'm here!")
+    
+def program2_4():
+    print("""Your assignment is to read "Hamlet" by tomorrow.""")
