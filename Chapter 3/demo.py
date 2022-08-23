7e08d71b410c6507bdd6f2208446e2eb72bbbdaf
NoahJack143 <111085947+NoahJack143@users.noreply.github.com>
2022-08-23 14:51:43 -0500

    Create demo.py

---
+++ Chapter 3/demo.py
@@ -0,0 +1,44 @@
+def demoprogram():
+#highlight and "tab" to tab everything
+#highlight and "shift+tab" to untab everything
+
+    value = int(input("Enter a vlue: "))
+    mystery_number = 393
+        
+    #See if the value is == the mystery number
+    if value == mystery_number:
+        print(" ")
+        print("The values match")
+            
+    print(" ")
+    print("Have a nice day")
+    
+def demoprogram2():
+#highlight and "tab" to tab everything
+#highlight and "shift+tab" to untab everything
+
+    bool("True")
+    
+    truth = bool(input("True or False?: "))
+        
+    #See if the value is == the mystery number
+    if truth:
+        print(" ")
+        print("True!")
+        
+def demoprogram3():
+#highlight and "tab" to tab everything
+#highlight and "shift+tab" to untab everything
+    
+    truth = bool(input("True or False?: "))
+    flag_var = True
+    
+    #See if the value is == the mystery number
+    if flag_var:
+        print(" ")
+        print("True!")
+    else:
+        print("False!")
+    
+
+
