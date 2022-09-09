7233bc5138774fa8b55d5d604a5b4cd70f83b891
NoahJack143 <111085947+NoahJack143@users.noreply.github.com>
2022-09-09 14:43:59 -0500

    Create function example.py

---
+++ Projects/function example.py
@@ -0,0 +1,6 @@
+#print line accepts the string message
+#it output the message using a wait command
+#it returns nothing
+def print_line(message):
+    #add your loop here to process message
+    print(message)
