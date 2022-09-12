095cb6242c3cac18f4dd9ca5042b2e08771d6cdd
NoahJack143 <111085947+NoahJack143@users.noreply.github.com>
2022-09-12 14:51:01 -0500

    Update Trimon.py

--- Projects/Trimon.py
+++ Projects/Trimon.py
@@ -5,8 +5,8 @@
 import time
 import turtle
 
-#Start the game
-def start_game():
+#Functions that will be used in the main function
+def start_game(): #This function will contain all the messages that will appear in the beginning of the game.
     intro_msg = 'Hello!'
     for letter in intro_msg:
         print(letter, end='')
@@ -14,6 +14,7 @@ def start_game():
     time.sleep(1.2)
     print(' ')
     print(' ')
+    
     intro_msg = 'Welcome to the world of Trimon!'
     for letter in intro_msg:
         print(letter, end='')
@@ -21,6 +22,7 @@ def start_game():
     time.sleep(1.2)
     print(' ')
     print(' ')
+    
     intro_msg = 'This world is populated with lots of Trimon that are ready to be caught by you!'
     for letter in intro_msg:
         print(letter, end='')
@@ -28,6 +30,7 @@ def start_game():
     time.sleep(1.2)
     print(' ')
     print(' ')
+    
     intro_msg = 'Before you can start your jounry, you must first tell us if you are a boy or a girl.'
     for letter in intro_msg:
         print(letter, end='')
@@ -36,14 +39,19 @@ def start_game():
     print(' ')
     print(' ')
     USER_INPUT = 0
+    
     #Ask the user to tell you if they're a boy or a girl
     while USER_INPUT == 0:
+        
+        #The beginning of the question
         intro_msg = 'Are you a boy or are you a girl'
         for letter in intro_msg:
             print(letter, end='')
             time.sleep(.03)
         gender = input('? ')
         print(' ')
+        
+        #Their first choice
         if gender == 'boy':
             intro_msg = """It's great to know you're a boy."""
             for letter in intro_msg:
@@ -53,6 +61,8 @@ def start_game():
             time.sleep(1.2)
             print(' ')
             print(' ')
+        
+        #Their second choice
         elif gender == 'girl':
             intro_msg = """It's great to know you're a girl."""
             for letter in intro_msg:
@@ -62,6 +72,8 @@ def start_game():
             time.sleep(1.2)
             print(' ')
             print(' ')
+            
+        #If they didn't choose either of the options
         elif gender != 'boy' and gender != 'girl':
             intro_msg = """Please enter either "boy" or "girl" """
             for letter in intro_msg:
@@ -71,7 +83,8 @@ def start_game():
             time.sleep(1.2)
             print(' ')
             print(' ')
-    #Extra information from the user that may or may not be used later on
+    
+    #Continue the questions that appear in the beginning of the game
     intro_msg = 'Now, please tell me your name'
     for letter in intro_msg:
         print(letter, end='')
@@ -80,6 +93,7 @@ def start_game():
     time.sleep(.5)
     print(' ')
     print(' ')
+    
     intro_msg = 'Great!'
     for letter in intro_msg:
         print(letter, end='')
@@ -87,6 +101,7 @@ def start_game():
     time.sleep(1.2)
     print(' ')
     print(' ')
+    
     intro_msg = 'Now, please tell us more about the boy living right next to you.'
     for letter in intro_msg:
         print(letter, end='')
@@ -94,6 +109,7 @@ def start_game():
     time.sleep(1.2)
     print(' ')
     print(' ')
+    
     RIVAL_NAME = 'none'
     intro_msg = 'What might his name be'
     for letter in intro_msg:
@@ -103,6 +119,7 @@ def start_game():
     time.sleep(.5)
     print(' ')
     print(' ')
+    
     intro_msg = """Now it's time for you to begin your journy."""
     for letter in intro_msg:
         print(letter, end='')
@@ -110,6 +127,7 @@ def start_game():
     time.sleep(1.2)
     print(' ')
     print(' ')
+    
     intro_msg = 'Be sure to catch lots of Trimon and fill the tridex'
     for letter in intro_msg:
         print(letter, end='')
@@ -118,21 +136,43 @@ def start_game():
     print(' ')
     print(' ')
     
-    #Begin using turtle to create the map for the user
+def begin_turtle(): #Begin using turtle to create the map for the user
     turtle.pendown()
     turtle.penup()
     turtle.speed(0)
     #Constants for turtle
-    X = -500
-    Y = -500
+    X = 0
+    Y = 0
     #Start by creating a grid for the map
     GRID_LOOP = 0
     while GRID_LOOP == 0:
         
+        #Horizontal lines + Constants
+        X = -500
+        Y = -500
         turtle.goto(X, Y)
         turtle.pendown()
         turtle.goto(X*-1, Y)
         turtle.goto(X, Y)
-        Y += 3
+        turtle.penup()
+        Y += 15
+        if turtle.pos
         
-start_game()
+        #Vertical lines
+        X = -500
+        Y = 500
+        turtle.goto(X, Y)
+        turtle.pendown()
+        turtle.goto(X, Y*-1)
+        turtle.goto(X, Y)
+        turtle.penup()
+        X += 15
+        
+        
+def Trimon(): #Main function to use
+    
+    #Beginning of the game messages
+    
+    
+    #Begin using turtle to show graphics for the user
+    begin_turtle()
