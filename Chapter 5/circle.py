b32084a853c89af6209aad05f3647432bac7093f
NoahJack143 <111085947+NoahJack143@users.noreply.github.com>
2022-09-14 14:49:24 -0500

    Create circle.py

---
+++ Chapter 5/circle.py
@@ -0,0 +1,15 @@
+#this circle module has functions that perform
+#calculations related to circles.
+import math
+
+def area(radius):
+    #area accepts an integer for radius
+    #it calculates and returns the area of a circle
+    
+    return math.pi * radius**2
+
+def circumference(radius):
+    #circumference accepts an argument for radius
+    #it calculates and returns the redius of a circle
+    
+    return 2 * math.pi * radius
